7bcf1ab159b3d047a97ef3c0f850a5a6300eb4d0
k-ye <yekuang.ky@gmail.com>
2017-07-15 13:29:53 -0700

    added ast for Scheme R1

---
+++ compiler/sch_ast.py
@@ -0,0 +1,72 @@
+class SchNode(object):
+    pass
+
+
+class SchExprNode(SchNode):
+    pass
+
+
+class SchIntNode(SchExprNode):
+
+    def __init__(self, x):
+        '''
+        x: A natural integer
+        '''
+        super(self, SchIntNode).__init__()
+        self._x = x
+
+    @property
+    def x(self):
+        return self._x
+
+
+class SchVarNode(SchExprNode):
+
+    def __init__(self, var):
+        '''
+        var: A string representing the variable name 
+        '''
+        super(self, SchVarNode).__init__()
+        self._var = var
+
+    @property
+    def var(self):
+        return self._var
+
+
+class SchApplyNode(SchExprNode):
+
+    def __init__(self, method, arg_list):
+        '''
+        method: A ?-node (or string for now?)
+        arg_list: A possibly empty list of SchExprNodes
+        '''
+        super(self, SchApplyNode).__init__()
+        self._method = method
+        self._arg_list = arg_list
+
+
+class SchLetNode(SchExprNode):
+
+    def __init__(self, var_list, body):
+        '''
+        var_list: A list of 2-tuple, where #1 is an SchVarNode and #2 an SchExprNode
+        body: An SchExprNode
+        '''
+        super(self, SchLetNode).__init__()
+        self._var_list = var_list
+        self._body = body
+
+
+class SchProgram(SchNode):
+
+    def __init__(self, expr):
+        '''
+        expr: An SchExprNode
+        '''
+        super(self, SchProgram).__init__()
+        self._expr = expr
+
+    @property
+    def program(self):
+        return self._expr
