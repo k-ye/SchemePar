86bf3f5a14ddac9206321532cf25a7e619b2fb88
k-ye <yekuang.ky@gmail.com>
2017-07-16 10:47:07 -0700

    move clean.sh to top level

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+find_cmd="-print"
+function find_files {
+    find -E . -type f -regex "(.*\.pyc)|(.*/parsetab\.py)|(.*/parser\.out)" $find_cmd
+}
+
+echo "Remove the following files:"
+find_files
+
+find_cmd="-delete"
+find_files
--- compiler/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -f *pyc parsetab.py* parser.out
