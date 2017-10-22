06de7fdd5d5c33d8ea006527050e34ce6a873741
k-ye <yekuang.ky@gmail.com>
2017-10-22 15:42:05 -0700

    runtime.c updated

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -1,15 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
 
-typedef int long long Ptr;
-
-Ptr read_int() {
-    Ptr x = 0;
+int64_t read_int() {
+    int64_t x = 0;
     // printf("Input integer: ");
     scanf("%lld", &x);
     return x;
 }
 
-void print_ptr(Ptr x) {
+void print_ptr(int64_t x) {
 	printf("%lld", x);
 	printf("\n");
 }
